eff540be23f87291a0a16c1b2a307ac5408c0188
shehtab <shehtabmasud10@gmail.com>
2023-03-22 00:27:28 -0400

    Worked on product page

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import Product from '../Product';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-products',
@@ -7,22 +10,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  querySub: Subscription[] = [];
+  products: Array<Product> = [];
+  featured: Array<Product> = [];
+  latest: Array<Product> = [];
+  showallproducts: boolean = false;
+  showlatestproducts: boolean = false;
+  showfeaturedproducts: boolean = false;
+
+  p:number = 1;
+  totalProduct:number = this.products.length;
+  itemsPerPage: number = 3;
+
+  constructor(private productServ: ProductService) { }
 
   ngOnInit(): void {
+      this.querySub.push(
+      this.productServ.getRecentProducts().subscribe(
+        (success) => {
+          this.products = success;
+          console.log("Successfully pulled recent products...")
+          console.log(success);
+
+          for(let i = this.products.length - 1; i > this.products.length - 6; i--)
+          {
+            this.latest.push(this.products[i]);
+          }
+          for(let i = this.products.length - 1; i > this.products.length - 16; i--)
+          {
+            this.featured.push(this.products[i]);
+          }
+        },
+        (err) => {
+          console.log("could not pull recent products...")
+          console.error(err);
+        }
+      )
+    )
   }
 
   showAllProducts() : void
   {
-    console.log("Show all prodcuts clicked");
+    this.showallproducts = true;
+    this.showlatestproducts = false;
+    this.showfeaturedproducts = false;
+  
   }
   showLatestProducts() : void
   {
-    console.log("Show Latest prodcuts clicked");
+    this.showallproducts = false;
+    this.showlatestproducts = true;
+    this.showfeaturedproducts = false;
   }
   showFeaturedProducts() : void
   {
-    console.log("Show Featured prodcuts clicked");
+    this.showallproducts = false;
+    this.showlatestproducts = false;
+    this.showfeaturedproducts = true;
   }
 
 }
